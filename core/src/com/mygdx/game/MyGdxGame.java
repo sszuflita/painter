57276b54592207e97ca474b0dd685f63cd7c1d2f
Sam Szuflita <sszuflit@caltech.edu>
2017-02-11 22:25:09 -0500

    Add keyboard inputs

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,11 +10,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int x;
+	int y;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("circle.jpg");
+		x = 0;
+		y = 0;
 	}
 
 	@Override
@@ -21,8 +26,20 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, .9f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			x -= 200 * Gdx.graphics.getDeltaTime();
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			x += 200 * Gdx.graphics.getDeltaTime();
+		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+			dispose();
+		}
+
+		batch.draw(img, x, y);
 		batch.end();
 	}
 	
