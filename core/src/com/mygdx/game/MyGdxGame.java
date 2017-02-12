fbe46285479d41de195515eefe0daa766add7108
Sam Szuflita <sszuflit@caltech.edu>
2017-02-11 22:51:51 -0500

    Add jumping

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -10,8 +10,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	int x;
-	int y;
+	float x;
+	float y;
+	float vx;
+	float vy;
+	float ax;
+	float ay;
+
+	boolean isJumping;
 	
 	@Override
 	public void create () {
@@ -19,28 +25,55 @@ public class MyGdxGame extends ApplicationAdapter {
 		img = new Texture("circle.jpg");
 		x = 0;
 		y = 0;
+		vx = 0;
+		vy = 0;
+		ax = 0;
+		ay = 0;
 	}
 
 	@Override
 	public void render () {
+
+
+		float delta = Gdx.graphics.getDeltaTime();
+
 		Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, .9f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-			x -= 200 * Gdx.graphics.getDeltaTime();
+			x -= 10 * delta;
 		}
 
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			x += 200 * Gdx.graphics.getDeltaTime();
+			x += 10 * delta;
 		}
+
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !isJumping) {
+			isJumping = true;
+			ay += 100;
+		} else if (y <= 0){
+			isJumping = false;
+		}
+
+		y = y + vy * delta;
+
+		vy = vy + ay * delta;
+
+		if (y < 0) {
+			y = 0;
+			ay = 0;
+			vy = 0;
+		} else {
+			ay = ay - 10;
+		}
+
+		batch.draw(img, x, y, 100, 100);
+		batch.end();
 
 		if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
 			dispose();
 		}
-
-		batch.draw(img, x, y);
-		batch.end();
 	}
 	
 	@Override
